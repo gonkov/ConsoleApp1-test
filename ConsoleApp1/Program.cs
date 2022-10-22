73d18585dc9c5e0151fd8223bf2613069f9dfba0
gonkov <115842637+gonkov@users.noreply.github.com>
2022-10-22 22:21:33 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            int j = 0;
+            while (j < 10)
+            {
+                j++;
+                Console.WriteLine(j);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
